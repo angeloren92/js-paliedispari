8df9c2435c354f97ae8e40d0bd40fa1a44ba8e16
angeloren92 <angeloren92@gmail.com>
2026-02-09 15:02:35 +0100

    PALI human code

---
+++ pali/js/script.js
@@ -0,0 +1,27 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma 
+
+Domande da  farsi quando si crea una funzione:
+Come dovrebbe chiamarsi? 
+Ho bisogno di parametri?
+Devo restituire un valore?
+Se sì, di che tipo?
+
+-Chiediamo all'utente di inserire una parola che andremo a controllare
+-Creiamo una funzione con un nome che ci permetta di capire che stiamo invertedo una parola
+    -la funzione ha bisogno di paramentri 
+    -la funzione deve invertire il contenuto della stringa andando ad iterare tante volte quanto la lunghezza della stringa
+    -la funzione deve restituire un valore che andreamo ad utilizzare successivamente
+-Creiamo una seconda funziona per verificare il risultato ottenuto con la parola iniziale per capire se sia palindroma o meno
+    -la funzione può sia restituire un valore da salvare e/o loggare fuori dalla fx, oppure possiamo dare l'istruzione di visualizzare direttamente in display
+-Abbiamo bisogno che ci venga restituito un valore booleano o un messaggio per capire se è palindroma la parola
+-->PS essendo case sensitive dobbiamo assicurarci che l'input corrisponda all'inversione, nel caso l'utente inserisca la parola con la maiuscola
+
+tools
+-variabili
+-prompt
+-console.log
+-funzione x2
+-if else
+*/
